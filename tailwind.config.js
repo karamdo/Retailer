4218e55a4772ddbb11457fd2e8913013848aae95
karamdo <k21r21m21@gmail.com>
2024-10-15 13:47:05 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,jsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+};
