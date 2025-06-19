d36118b6f636573681f16fad59f23d82725a321f
karamdo <k21r21m21@gmail.com>
2025-06-19 04:47:30 +0300

    Updates

---
+++ assets/Spinner-ByYXkgSs.js
@@ -0,0 +1,1 @@
+import{j as s,s as i}from"./index-CYO_5tON.js";function r({size:e="md",className:n=""}){const t={sm:"w-4 h-4",md:"w-8 h-8",lg:"w-12 h-12",xl:"w-16 h-16"};return s.jsx("div",{className:`flex items-center justify-center ${n}`,children:s.jsx(i,{className:`${t[e]} animate-spin text-blue-500`})})}export{r as S};
